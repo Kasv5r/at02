0b523933d44e65ad0cbc4ce53f4d724e317f694b
Kasv5r <mauuuueretetw@gmail.com>
2023-08-07 20:47:01 -0300

    Métodos getter e setters implementados

--- at02.dart
+++ at02.dart
@@ -1,3 +1,9 @@
 class Teste {
-  var publicos, estudante, nota;
+  var estudante, nota;
+
+  String getEstudante() => this.estudante;
+  double getNota() => this.nota;
+
+  void setEstudante(String est) => this.estudante = est;
+  void setNota(double nt) => this.nota = nt;  
 }
